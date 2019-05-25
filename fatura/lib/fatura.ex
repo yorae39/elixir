85fc5c955b43035faec99dae3beba23c665cfaa9
Luiz Paulo <lpaureliano74@gmail.com>
2019-05-25 20:50:41 -0300

    Melhoria no doc

--- fatura/lib/fatura.ex
+++ fatura/lib/fatura.ex
@@ -1,8 +1,21 @@
 defmodule Fatura do
-  def criar_fatura do
-    ["Telefone", "Luz", "Agua"]
-  end
 
+  @moduledoc """
+    Neste módulo executamos funções de fatura
+  """
+
+  @doc """
+    Ao receber uma `fatura` retorna um array de faturas
+      ## Exemplos
+      iex> Fatura.criar_fatura(["Telefone", "Agua", "Luz"])
+      ["Telefone", "Agua", "Luz"]
+  """
+  def criar_fatura(fatura) do
+    fatura
+  end
+@doc """
+  Ao receber `faturas` ordena as mesmas
+"""
   def ordena_fatura(faturas) do
     Enum.sort(faturas)
   end
