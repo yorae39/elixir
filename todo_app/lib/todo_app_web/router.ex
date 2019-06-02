2d12fb26a8ec11167cccbfa2ddfa473b7f5270b9
Luiz Paulo <lpaureliano74@gmail.com>
2019-06-02 15:52:49 -0300

    Routes e melhorando a aparencia

--- todo_app/lib/todo_app_web/router.ex
+++ todo_app/lib/todo_app_web/router.ex
@@ -19,6 +19,9 @@ defmodule TodoAppWeb.Router do
     get "/new", TarefaController, :new
     post "/", TarefaController, :create
     get "/", TarefaController, :index
+    get "/:id/edit", TarefaController, :edit
+    put "/:id", TarefaController, :update
+
   end
 
   # Other scopes may use custom stacks.
